0c8c01a9d181920e2a8e46605e441d4379d1ca7c
lemon8888 <lemon12321@163.com>
2020-02-07 17:24:18 +0800

    027

---
+++ 027remove-element.c
@@ -0,0 +1,10 @@
+//ÒÆ¶¯¿ìÂıÖ¸Õë
+int removeElement(int* nums, int numsSize, int val){
+    int i=0;
+    for(int j=0;j<numsSize;j++){
+        if(nums[j]!=val){
+            nums[i++]=nums[j];
+        }
+    }
+    return i;
+}
