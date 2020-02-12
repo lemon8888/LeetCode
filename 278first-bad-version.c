dfe15f7c01ae1887f0f8526fdcdbfe5e1b6e48f5
lemon8888 <lemon12321@163.com>
2020-02-12 11:49:34 +0800

    278

---
+++ 278first-bad-version.c
@@ -0,0 +1,19 @@
+//二分法
+bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int i=1,mid;
+    while(i<=n){
+        mid=i+(n-i)/2;
+        if(isBadVersion(mid)){
+            if(!isBadVersion(mid-1)){
+                return mid;
+            }else{
+                n=mid-1;
+            }
+        }else{
+            i=mid+1;
+        }
+    }
+    return -1;
+}
