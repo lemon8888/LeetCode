a7cbdf0d98ad4278ad74e55c1a37f95dfe3d1a2a
lemon8888 <lemon12321@163.com>
2020-02-18 23:06:16 +0800

    001

--- 001two-sum.c
+++ 001two-sum.c
@@ -1,6 +1,6 @@
-一
+//暴力法
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    *returnSize=0;;
+    *returnSize=0;
     int* res=(int *)malloc(sizeof(int)*2);
     for(int i=0;i<numsSize-1;i++){
         for(int j=i+1;j<numsSize;j++){
