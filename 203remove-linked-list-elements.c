5c420565ee865d01653a273dcfe764b8412bc86d
lemon8888 <lemon12321@163.com>
2020-02-27 22:38:51 +0800

    203

---
+++ 203remove-linked-list-elements.c
@@ -0,0 +1,19 @@
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode *p,*s;
+    while(head&&head->val==val){
+        s=head;
+        head=head->next;
+        free(s);
+    }
+    p=head;
+    while(p){
+        if(p->next&&p->next->val==val){
+            s=p->next;
+            p->next=p->next->next;
+            free(s);
+        }else{
+            p=p->next;
+        }
+    }
+    return head;
+}
