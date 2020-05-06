10812f5514c84e306578f021e4bb27bdc49493d3
lemon8888 <lemon12321@163.com>
2020-05-06 21:40:40 +0800

    0009

---
+++ 0009-palindrome-number.c
@@ -0,0 +1,12 @@
+//翻转比较是否相同
+bool isPalindrome(int x){
+    if(x<0){
+        return false;
+    }
+    long temp=x,y=0;
+    while(temp){
+        y=y*10+temp%10;
+        temp/=10;
+    }
+    return x==y;
+}
