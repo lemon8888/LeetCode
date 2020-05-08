f5e4452582fe8d440be34b24831c7de937c0a7a7
lemon8888 <lemon12321@163.com>
2020-05-08 22:59:52 +0800

    0509

---
+++ 0509-fibonacci-number.c
@@ -0,0 +1,29 @@
+//动态规划
+//自底向上
+int fib(int N){
+    if(N<=1){
+        return N;
+    }
+    int num[N+1];
+    num[0]=0;
+    num[1]=1;
+    for(int i=2;i<=N;i++){
+        num[i]=num[i-1]+num[i-2];
+    }
+    return num[N];
+}
+
+
+//迭代
+int fib(int N){
+    if(N<=1){
+        return N;
+    }
+    int a=0,b=1,num;
+    for(int i=2;i<=N;i++){
+        num=a+b;
+        a=b;
+        b=num;
+    }
+    return num;
+}
