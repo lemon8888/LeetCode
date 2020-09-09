c23d89f21ca36f27d41a81133e757b955c091417
lemon8888 <lemon12321@163.com>
2020-09-09 23:07:19 +0800

    update

---
+++ 02-02-kth-node-from-end-of-list-lcci.c
@@ -0,0 +1,12 @@
+int kthToLast(struct ListNode* head, int k){
+    struct ListNode *p,*q;
+    p=q=head;
+    for(k;k>1;k--){
+        p=p->next;
+    }
+    while(p->next!=NULL){
+        p=p->next;
+        q=q->next;
+    }
+    return q->val;
+}
